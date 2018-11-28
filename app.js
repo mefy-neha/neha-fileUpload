cfece2e579d5b17fce5d3a9f04ebd29f131c2173
meme-neha <neha.keshri@memeinfotech.com>
2018-11-28 15:34:28 +0530

    use some library

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
 mongoose.connect('mongodb://127.0.0.1:27017/neha-fileUpload',{useNewUrlParser: true },(err,res)=>{
 console.log('errrr',err)
 // console.log('resss',res)

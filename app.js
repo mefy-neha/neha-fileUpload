19477ceaab9755387f8d88fb39934c933e342c21
Pushpendu <contactpushpendu@gmail.com>
2018-11-29 16:40:00 +0530

    app js changed

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var dotenv = require('dotenv').config();
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var mongoose = require('mongoose');
 var app = express();
 
+var app = require('../app');
+var port = normalizePort(process.env.PORT || '5000');
+app.set('port', port);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 mongoose.set('useCreateIndex', true);
 
-mongoose.connect('mongodb://127.0.0.1:27017/neha-fileUpload',{ autoIndex: false },(err,res)=>{
-console.log('errrr',err)
-// console.log('resss',res)
+mongoose.connect('mongodb://127.0.0.1:27017/neha-fileUpload', { autoIndex: false }, (err, res) => {
+  console.log('errrr', err)
+  // console.log('resss',res)
 
 });
 // uncomment after placing your favicon in /public
@@ -64,3 +67,80 @@ mongoose.connection.on('error', (err) => {
   }
 });
 module.exports = app;
+
+
+app.set('port', port);
+console.log('Server is running', port)
+/**
+ * Create HTTP server.
+ */
+
+var server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  debug('Listening on ' + bind);
+}
